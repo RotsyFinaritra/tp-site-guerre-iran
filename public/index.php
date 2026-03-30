7404474470b34e027a98d0dc910ccfc111ec7afe
mirana-voniarintsoa <miranavoniarintsoa@gmail.com>
2026-03-30 20:12:48 +0300

    redesign page details article

--- public/index.php
+++ public/index.php
@@ -1,11 +1,6 @@
-<!doctype html>
-<html lang="fr">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Frontoffice</title>
-</head>
-<body>
-	<a href="/views/home.php">Aller à l’accueil</a>
-</body>
-</html>
+<?php
+
+declare(strict_types=1);
+
+header('Location: /views/home.php', true, 302);
+exit;
